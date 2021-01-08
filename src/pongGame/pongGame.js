4648eb6cc9f624cc1878c34ad3b21363e080f09b
Francisco <francisco59553@gmail.com>
2021-01-08 16:51:01 +0100

    docs: adding comments to pongGame.js

--- src/pongGame/pongGame.js
+++ src/pongGame/pongGame.js
@@ -38,48 +38,56 @@ var scoreP1Text;
 var scoreP2Text;
 
 function createGame() {
-    cursor = this.input.keyboard.createCursorKeys();
 
+    // Define keys to move paddles
+    cursor = this.input.keyboard.createCursorKeys();
+    keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
+    // Define scores Text
     scoreP1Text = this.add.text(16, 20, 'Score : 0', { font: 'bold 16px Courier', fontSize: '16px', fill: '#d41f1f' });
     scoreP2Text = this.add.text(700, 20, 'Score : 0', { font: 'bold 16px Courier', fontSize: '16px', fill: '#32a852' });
 
+    // Define the ball and its physic
     ball = this.physics.add.sprite(400, 300, 'ball');
     ball.setCollideWorldBounds(true);
     ball.setBounce(1);
     ball.setVelocityX(velocityBallX);
     ball.setVelocityY(velocityBallY);
 
+    // Draw rectangles
     var rectTopPlayer1 = this.add.rectangle(0, -30, 10, 60, 0xd41f1f); // rouge
     var rectBotPlayer1 = this.add.rectangle(0, 30, 10, 60, 0xd41f1f); // rouge
-
     var rectTopPlayer2 = this.add.rectangle(0, -30, 10, 60, 0x32a852); // vert
     var rectBotPlayer2 = this.add.rectangle(0, 30, 10, 60, 0x32a852); // vert
 
+    // Add physics to rectangles
     this.physics.add.existing(rectTopPlayer1);
     this.physics.add.existing(rectBotPlayer1);
     this.physics.add.existing(rectTopPlayer2);
     this.physics.add.existing(rectBotPlayer2);
 
+    // Define containers which contains rectangles
     containerPlayer1 = this.add.container(10, 300, [rectTopPlayer1, rectBotPlayer1]);
     containerPlayer1.setSize(10, 120);
-
     containerPlayer2 = this.add.container(790, 300, [rectTopPlayer2, rectBotPlayer2]);
     containerPlayer2.setSize(10, 120);
 
+    // Add physics to containers
     this.physics.world.enable(containerPlayer1);
     this.physics.world.enable(containerPlayer2);
 
-    keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-
+    // Set rectangles not pushable
     rectTopPlayer1.body.pushable = false;
     rectBotPlayer1.body.pushable = false;
     rectTopPlayer2.body.pushable = false;
     rectBotPlayer2.body.pushable = false;
 
+    // Set containers collide world bounds
     containerPlayer1.body.collideWorldBounds = true;
     containerPlayer2.body.collideWorldBounds = true;
 
+    // Add collision events to rectangles
     this.physics.add.collider(ball, rectTopPlayer1, hitPlayer1Top, null, this);
     this.physics.add.collider(ball, rectBotPlayer1, hitPlayer1Bot, null, this);
     this.physics.add.collider(ball, rectTopPlayer2, hitPlayer2Top, null, this);
@@ -91,6 +99,7 @@ function updateGame() {
     playersMoves.movePlayer1();
     playersMoves.movePlayer2();
 
+    // Define the score when the ball reach world bounds (left and right)
     if (ball.x > 780) {
         scoreP1++;
         scoreP1Text.setText(`Score : ${scoreP1}`);
@@ -104,6 +113,7 @@ function updateGame() {
     }
 }
 
+// Reset ball options when a player win a round
 function reset() {
     ball.x = 400;
     ball.y = 300;
@@ -137,11 +147,13 @@ function hitPlayer2Bot() {
     hitPlayer();
 }
 
+// Function called when ball collide a paddle
 const hitPlayerFun = require('./hitPlayer.js');
 function hitPlayer() {
     hitPlayerFun.hitPlayer();
 }
 
+// Define the default direction of the ball at start randomly
 function getRandomVelocity() {
     let rand = Math.floor(Math.random() * 50) + 50;
     return Math.random() < 0.5 ? rand * -1 : rand;
